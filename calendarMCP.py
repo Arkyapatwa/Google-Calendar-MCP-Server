d5beaf6e0be7252b7b575ba0f89ab54aceeca417
Arkya Patwa <arkyapatwa333@gmail.com>
2025-03-29 22:00:43 +0530

    list events of calendar done

--- calendarMCP.py
+++ calendarMCP.py
@@ -1,12 +1,14 @@
 from mcp.server.fastmcp import FastMCP
+import logging
 
-from models import ListEventParameters
+from models.ListEventParameters import ListEventsParameters
+from services.events import list_calendar_events
 
 mcp = FastMCP("google-calendar")
 
 # Events API
 @mcp.tool("list-events")
-def list_events(listEventParameters: ListEventParameters):
+def list_events(listEventParameters: ListEventsParameters = ListEventsParameters()):
     """
     Gives the list of events from the primary calendar
     
@@ -15,19 +17,26 @@ def list_events(listEventParameters: ListEventParameters):
     Returns:
         A list of events
     """
-    pass
+    try:
+        return list_calendar_events(listEventParameters)
+    except Exception as e:
+        logging.error(f"Failed to fetch calendar events: {e}")
+        return []
 
-@mcp.tool("insert-event")
-def insert_event():
-    pass
+# @mcp.tool("insert-event")
+# def insert_event():
+#     """
+#     Inserts a event in the calendar
+#     """
+#     pass
 
-@mcp.tool("delete-event")
-def delete_event():
-    pass
+# @mcp.tool("delete-event")
+# def delete_event():
+#     pass
 
-@mcp.tool("update-event")
-def update_event(): 
-    pass
+# @mcp.tool("update-event")
+# def update_event(): 
+#     pass
 
 
 if __name__ == '__main__':
--- services/events.py
+++ services/events.py
@@ -1,5 +1,17 @@
 from models import ListEventParameters
-from typing import List
+from google_calendar_connection import GoogleCalendarService
 
-async def list_calendar_events(parameters: ListEventParameters):
-    pass
+from typing import List
+import logging
+
+def list_calendar_events(parameters: ListEventParameters):
+    calendar_service = GoogleCalendarService().get_service()
+    
+    try:
+        params = {k: v for k, v in parameters.__dict__.items() if v is not None}
+        
+        events_result = calendar_service.events().list(**params)
+        return events_result.execute().get('items', [])
+    except Exception as e:
+        logging.error(f"Failed to fetch calendar events: {e}")
+        return []
